e02411e7c7f2acc6c9302f313b7cdfcdceb77f56
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-20 17:38:46 +0300

    feat: dotenv

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,14 +38,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = require("./routes/routes");
 const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
 const app = (0, express_1.default)();
 const { PORT = 3000 } = process.env;
+const { DB_HOST = '127.0.0.1' } = process.env;
+const { DB_PORT = '27017' } = process.env;
+const { DB_NAME = 'kspdb' } = process.env;
 mongoose_1.default.set('strictQuery', false);
 app.use(routes_1.pagesRouter);
 function start() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            yield mongoose_1.default.connect('mongodb://127.0.0.1:27017/kspdb');
+            yield mongoose_1.default.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
             app.listen(PORT, () => {
                 console.log(`Server has been started on port ${PORT} !`);
             });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import { pagesRouter } from './routes/routes';
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv'
 
+
+dotenv.config();
 const app = express();
+
+
 const {PORT = 3000} =  process.env;
+const { DB_HOST = '127.0.0.1' } = process.env;
+const { DB_PORT = '27017' } = process.env;
+const { DB_NAME = 'kspdb' } = process.env;
 
 mongoose.set('strictQuery', false);
 
-
 app.use(pagesRouter);
-
 
 async function start() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/kspdb');
+    await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
     app.listen(PORT, () => {
       console.log(`Server has been started on port ${PORT} !`)
     });
